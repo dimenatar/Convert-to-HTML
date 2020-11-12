8111b02d911a2fb2c853fd41f255ce63e4bbb602
dimenatar <71691899+dimenatar@users.noreply.github.com>
2020-11-12 20:19:05 +0300

    Final?

---
+++ HTMLC/HTMLC/HTMLGetter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace HTMLC
+{
+    static class HTMLGetter
+    {
+        static private string text;
+        static public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        static public string TransformSU(string Text)
+        {
+
+            Regex regex = new Regex("^su | su$|\\ssu\\s|^su\\b| su\\b");
+            bool First = true;
+
+            while (regex.IsMatch(Text))
+                    if (First)
+                    {
+                        Text = regex.Replace(Text, "<sub>", 1);
+                        First = false;
+
+                    }
+                    else
+                    {
+                            First = true;
+                            Text = regex.Replace(Text, "</sub>", 1);
+                    }
+
+            return Text;
+        }
+
+        static public string[] TransformFont(string Text)
+        {
+            string[] splitText = Text.Split('\n');
+            string Temp = "";
+            Regex regex = new Regex("^Fc\\w{1}");
+            for (int i =0; i < splitText.Length; i ++)
+            {
+                if (regex.IsMatch(splitText[i]))
+                {
+                    
+                    Temp = splitText[i];
+                    if (Temp[2] == 'A')
+                    {
+                        splitText[i] = regex.Replace(splitText[i], "<font color= Aqua>");
+                        splitText[i] += "</font>\n";
+                    }
+                    else if (Temp[2] == 'P')
+                    {
+                        splitText[i] = regex.Replace(splitText[i], "<font color= Purple>");
+                        splitText[i] += "</font>\n";
+                    }
+                    else if (Temp[2] == 'G')
+                    {
+                        splitText[i] = regex.Replace(splitText[i], "<font color= Gold>");
+                        splitText[i] += "</font>\n";
+                    }
+                    else if (Temp[2] == 'O')
+                    {
+                        splitText[i] = regex.Replace(splitText[i], "<font color= Orange>");
+                        splitText[i] += "</font>\n";
+                    }
+                    else
+                    {
+                        splitText[i] = regex.Replace(splitText[i], "<font color= Pink>");
+                        splitText[i] += "</font>\n";
+                    }
+                }
+            }
+            return splitText;
+        }
+        public static void GreateHtmlDoc(string TextInBody)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html >");
+            sb.Append("<head >");
+            string meta = @"<meta charset=""UTF-8"">";
+            sb.Append(meta);
+            sb.Append("<title >");
+            sb.Append("</title >");
+            sb.Append("</head >");
+            sb.Append("<body >");
+            sb.Append(TextInBody);
+            sb.Append("</body >");
+            sb.Append("</html >");
+            using (StreamWriter sw = new StreamWriter("MyHtml.html"))
+            {
+                sw.Write(sb.ToString());
+                sw.Close();
+                Console.WriteLine("Файл создан успешно!");
+            }
+        }
+
+    }
+}
---
+++ HTMLC/HTMLC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HTMLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullHTML = "";
+            string text = Console.ReadLine();
+            
+           text = HTMLGetter.TransformSU(text);
+            Console.WriteLine(text);
+            ///string secondtext = Console.ReadLine();
+            //string[] splittext = HTMLGetter.TransformFont(secondtext);
+            string[] splittext = HTMLGetter.TransformFont("FcA Я хочу пиццы\nFcO актоАКТОакто\nFcGКурага_Угувуга\nFcPС плюс минус ?");
+            for (int i =0; i < splittext.Length; i++)
+            {
+                FullHTML += splittext[i];
+                Console.WriteLine(splittext[i]);
+            }
+            FullHTML += text;
+            HTMLGetter.GreateHtmlDoc(FullHTML);
+        }
+    }
+}
